cc87563a64478be77462a8d61f5f972afc7d2fd5
Chrille <Chrille@192.168.0.30>
2015-10-21 22:10:21 +0200

    Added, file reading and file writing to highscores!

---
+++ EarthInvasion/src/view/FileHandler.java
@@ -0,0 +1,85 @@
+package view;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Scanner;
+import java.util.StringTokenizer;
+
+/**
+ * The class FileHandler contains read and write methods. The class read and
+ * writes to a specific file called "booklist.ser", if the file is not found it
+ * will create a new file with that specific name.
+ *
+ * @author Christian Ekenstedt & Joachim Zetterman
+ */
+@SuppressWarnings("unchecked")
+public class FileHandler {
+    static String newline = System.getProperty("line.separator");
+
+    /**
+     * The method read reads the file "booklist.ser" and deserializes the data.
+     * If the file is not found, it create a new .ser file and returns null.
+     *
+     * @return books, a reference to a temporary list of the type Book. Returns
+     * null if the file is not found.
+     * @throws Exception
+     */
+    public String read() throws Exception {
+        String line;
+        String name;
+        int score;
+        StringTokenizer tokenizer;
+        String highscoreString = "";
+
+        BufferedReader fin = null;
+
+        try {
+            fin = new BufferedReader(new FileReader("highscores.txt"));
+
+            line = fin.readLine(); 	// Read a line from the file...
+            while (line != null) {	// ...while there's more to read.
+
+                try {
+                    tokenizer = new StringTokenizer(line);
+                    highscoreString += tokenizer.nextToken() + "\t\t" + tokenizer.nextToken() + "\t\t" + tokenizer.nextToken() + "\n";
+                } catch (NumberFormatException ne) {
+                    System.out.println("Input mismatch. Skipping this line.");
+                }
+                line = fin.readLine();  // Read another line	
+            }
+        } catch (FileNotFoundException fe) {
+            System.out.println("Can not find the specified file.");
+            throw fe; // "do not nail your corpse in the upright position"
+        } finally {
+            try {
+                if (fin != null) {
+                    fin.close();
+                }
+            } catch (IOException ie) {
+            }
+            
+        }
+        return highscoreString;
+
+    }
+
+    /**
+     * The method write serializes data to the file booklist.ser
+     *
+     * @throws Exception
+     */
+    public void write(String highscoresString) throws Exception {
+        PrintWriter fout = null;
+        FileWriter fw = null;
+        try {
+            fw = new FileWriter("highscores.txt");
+            fout = new PrintWriter(new BufferedWriter(fw));
+            // Writing to file...
+            fout.println(highscoresString);
+        } finally {
+            if (fout != null) {
+                fout.close();
+            }
+        }
+    }
+}
--- EarthInvasion/src/view/HighscoreView.java
+++ EarthInvasion/src/view/HighscoreView.java
@@ -9,6 +9,10 @@ import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
 
@@ -20,30 +24,45 @@ public class HighscoreView {
     private Stage highStage;
     private BorderPane highPane;
     private Scene scene;
+    private FileHandler highscoreList;
     
-    public HighscoreView(){
+    public HighscoreView() throws Exception{
+        highscoreList = new FileHandler();
         highStage = new Stage();
         highPane = new BorderPane();
-        scene = new Scene(highPane, 400, 400);
+        scene = new Scene(highPane, 300, 300);
         highPane.setPadding(new Insets(10, 10, 10, 10));
-        initHighscoreWindow();
+        
+        initHighscoreWindow(highscoreList.read());
     }
     
-    private void initHighscoreWindow() {
+    private void initHighscoreWindow(String highscoreString) {
         Label title = new Label("Highscores");
-        Label info = new Label("highscores will be here");
+        title.setFont(Font.font("Helvetica",FontWeight.BOLD, 20));
+        title.setUnderline(true);
+        Label border = new Label("\nName\t\tLevel\tScore\n");
+        border.setFont(Font.font("Helvetica",FontWeight.BOLD,15));
+        Label info = new Label(highscoreString);
+        info.setFont(Font.font("Helvetica", 15));
         
         Button okButton = new Button("Thanks, now i will try to beat that!");
         okButton.autosize();
-        HBox buttons = new HBox(5);
-        title.setCenterShape(true);
         
+        HBox buttons = new HBox(5);
         buttons.getChildren().add(okButton);
         buttons.setAlignment(Pos.BOTTOM_CENTER);
-        highPane.setBottom(buttons);
         
+        VBox texts = new VBox(5);
+        texts.getChildren().addAll(border,info);
+        
+        
+        
+        
+        
+        highPane.setBottom(buttons);
         highPane.setTop(title);
-        highPane.setCenter(info);
+        highPane.setCenter(texts);
+        highPane.setAlignment(title,Pos.TOP_CENTER);
 
         okButton.setOnAction(new EventHandler<ActionEvent>() {
             @Override
@@ -58,11 +77,13 @@ public class HighscoreView {
 
     public void showWindow() {
         highStage.setTitle("Rules!");
+        scene.setFill(Color.WHITESMOKE);
         highStage.setScene(scene);
         highStage.setResizable(false);
         highStage.setAlwaysOnTop(false);
         highStage.toFront();
         highStage.initStyle(StageStyle.UNDECORATED);
+        
 
         highStage.showAndWait();
         //Här borde vi lägga någon typ av return
